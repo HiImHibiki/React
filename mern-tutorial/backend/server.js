02ef5ac8afd126fd92e26234e6d28df6a837e066
Ryoma Shu <mario.rufisanto@gmail.com>
2022-03-13 04:25:32 +0700

    Goals controller & routes setup

--- mern-tutorial/backend/server.js
+++ mern-tutorial/backend/server.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json()); // untuk baca req body json
+app.use(express.urlencoded({ extended: false })); // untuk baca req body urlencoded
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
